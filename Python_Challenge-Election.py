2ff6f77406118d71e88196c75268a594979aaa9e
TinTesla <125936856+TinTesla@users.noreply.github.com>
2023-03-23 18:10:05 -0700

    Final Final

--- Python_Challenge-Election.py
+++ Python_Challenge-Election.py
@@ -14,7 +14,7 @@ budget_path = os.path.join('Resources', 'election_data.csv')
 with open(budget_path, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
-        #building lists
+        #building list
         el_canidate.append(row[2])
 
 #counting votes
@@ -27,12 +27,6 @@ for x in el_canidate:
     elif x == "Raymon Anthony Doane":
         votes_raymon = votes_raymon + 1
 
-#print check
-    #print(total_votes)
-    #print(votes_charles)
-    #print(votes_diana)
-    #print(votes_raymon)
-
 #percent calculations
 p_charles = round(votes_charles / total_votes *100, 2)
 p_charles = str(p_charles) + "%"
@@ -41,11 +35,6 @@ p_diana = str(p_diana) + "%"
 p_raymon = round(votes_raymon / total_votes *100, 2)
 p_raymon = str(p_raymon) + "%"
 
-#print check
-    #print(p_charles)
-    #print(p_diana)
-    #print(p_raymon)
-
 #winner check
 if votes_charles > votes_diana and votes_charles > votes_raymon:
     votes_winner = "Charles Casper Stockham"
@@ -53,9 +42,8 @@ elif votes_diana > votes_charles and votes_diana > votes_raymon:
     votes_winner = "Diana DeGette"
 elif votes_raymon > votes_charles and votes_raymon > votes_diana :
     votes_winner = "Raymon Anthony Doane"
-#print(votes_winner)
 
-
+#txt output
 with open('Analysis/Election_Analysis.txt', 'w') as f:
     f.write(f"Election Results\n")
     f.write(f"---------------------------\n")
@@ -66,23 +54,3 @@ with open('Analysis/Election_Analysis.txt', 'w') as f:
     f.write(f"Raymon Anthony Doane: {str(p_raymon)}\n") 
     f.write(f"---------------------------\n")           
     f.write(f"Winner: {str(votes_winner)}\n")
-
-
-#election_data setup:
-#column 1 = Ballot ID
-#column 2 = Country
-#column 3 = Canidate
-#row 1 = Titles
-#rows 2 to 369712 = Data
-
-#election_data Goal:
-#Header: "Election Results"
-#Seperator: "---------------------------"
-#Total Votes: >>(sum of unique rows in column 1)
-#Seperator: "---------------------------"
-#"Charles Casper Stockham: " + [(Countif column 3 = Charles Casper Stockham)/ sum of column 3] + "%" + "(" + Countif column 3 = Charles Casper Stockham + ")"
-#"Diana DeGette: " + [(Countif column 3 = iana DeGette)/ sum of column 3] + "%" + "(" + Countif column 3 = Diana DeGette + ")"
-#"Raymon Anthony Doane: " + [(Countif column 3 = Raymon Anthony Doane)/ sum of column 3] + "%" + "(" + Countif column 3 = Raymon Anthony Doane + ")"
-#Seperator: "---------------------------"
-#"Winner: " + ?? Entry with most results in column 3
-#export results into Analysis as file "Election_Analysis.txt"
--- Python_Challenge-Financial.py
+++ Python_Challenge-Financial.py
@@ -22,7 +22,6 @@ with open(budget_path, 'r') as csvfile:
 #counting dates
 for x in budget_date:
     total_months = total_months + 1
-#print(total_months)
 
 #profit/ loss calculations
 for i  in budget_pl[1:]:
@@ -33,13 +32,6 @@ for i  in budget_pl[1:]:
         grt_dc = int(i)
 avg_change = total_total / total_months
 
-#print check
-#print(total_total)
-#print(total_months)
-#print(avg_change)
-#print(grt_in)
-#print(grt_dc)
-
 #txt output
 with open('Analysis/Financial_Analysis.txt', 'w') as f:
     f.write(f"Financial Analysis\n")
@@ -49,25 +41,3 @@ with open('Analysis/Financial_Analysis.txt', 'w') as f:
     f.write(f"Average Change: {str(avg_change)}\n")
     f.write(f"Greatest Increase in Profits: {str(grt_in)}\n")
     f.write(f"Greatest Decrease in Profits: {str(grt_dc)}\n")
-
-
-#output_file = "Analysis/Financial_Analysis.txt"
-#with open(output_file, 'w') as file_writter:
-    #writter = 
-
-
-#budget_data setup:
-#column 1 = Date
-#column 2 = Profit/ loss
-#row 1 = Titles
-#Rows 2 to 87 = Data
-
-#budget_data Goal:
-#Header: "Financial Analysis"
-#Seperator: "---------------------------"
-#"Total Months: " + (# of unique rows in column 1)
-#"Total: " + (sum of column 2)
-#"Average Change: " + "$" + (average of column 2)
-#"Greatest Increase in Profits: " + ?? Date X ($X) ?? {#1 Header?}
-#"Greatest Decrease in Profits: " + ?? Date X ($X) ?? {#1 Tail?}
-#export results into Analysis as file "Financial_Analysis.txt"
